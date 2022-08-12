91eee10fc1c150b40c303af6ac3a433416ab11ff
Alina Aftab <alina.aftab2001@gmail.com>
2022-08-12 10:11:05 +0500

    logged in users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth');
 
 // @ROUTE   GET API/PROFILE
 // @DESC    TEST ROUTE
 // @ACCESS  PUBLIC
-router.get('/',(req,res) => res.send('Profile Route'));
+router.get('/me',auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'gravatar']);
+
+        if (!profile) return res.status(400).send('There is no profile for this user');
+        
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
+
+});
 
 module.exports = router;
