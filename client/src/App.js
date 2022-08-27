39ce70b309594571c30759f6c9d3008513e7205b
Alina Aftab <alina.aftab2001@gmail.com>
2022-08-27 15:37:16 +0500

    lalala

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,43 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
-import React, { Fragment } from 'react';
+import Dashboard from './components/dasboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import React, { Fragment, useEffect } from 'react';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <section>
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </section>
-    </Fragment>
-  </Router>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <section className='container'>
+            <Alert />
+            <Routes>
+              <Route path='/' element={<Landing />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+              <PrivateRoute path='/dashboard' element={<Dashboard />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
